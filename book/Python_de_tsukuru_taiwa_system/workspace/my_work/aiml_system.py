cf6145487f72d0bc5fd69ee99a203a9e446d4073
B.B <b.b@biebienoMacBook-Air.local>
2021-03-13 17:09:34 +0900

    Python_de_tsukuru_taiwa_system 3.2 done

---
+++ book/Python_de_tsukuru_taiwa_system/workspace/my_work/aiml_system.py
@@ -0,0 +1,35 @@
+import aiml
+import MeCab
+from telegram_bot import TelegramBot
+
+class AimlSystem:
+    def __init__(self):
+        # AIMLを読み込むためのインスタンスを用意
+        self.kernel = aiml.Kernel()
+        # 形態素解析器を用意
+        self.tagger = MeCab.Tagger('-Owakati')
+
+    def initial_message(self):
+        # aiml.xmlを読み込む
+        self.kernel.learn("aiml.xml")
+        return {'utt':'はじめまして，雑談を始めましょう', 'end':False}
+
+    def reply(self, dic_input):
+        utt = dic_input['utt']
+        utt = self.tagger.parse(utt)
+        # 対応するセッションのkernelを取り出し，respondでマッチするルールを探す
+        response = self.kernel.respond(utt)
+        # print(utt, response)
+        return {'utt': response, 'end':False}
+
+if __name__ == '__main__':
+    system = AimlSystem()
+    dic_input = system.initial_message()
+    #print('dic_input', dic_input)
+    print("bot>"+dic_input['utt'])
+
+    while True:
+        raw_input = str(input("me>"))
+        dic_input['utt'] = raw_input
+        dic_input = system.reply(dic_input)
+        print("bot>"+dic_input['utt'])
